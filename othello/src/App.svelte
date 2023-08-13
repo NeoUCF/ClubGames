716647afca350fc8d55d9459d3297d081d772d4c
Neo <neocamanga@knights.ucf.edu>
2023-08-13 16:29:13 -0400

    init

---
+++ othello/src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Counter from "./lib/Counter.svelte";
+	import Board from "./components/Board.svelte";
+</script>
+
+<main>
+	<div class="card">
+		<Counter />
+		<Board />
+	</div>
+</main>
+
+<style>
+</style>
